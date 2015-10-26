ed4b328dbcd826194f626084feee50645055fd8d
Franton Lin <franton@mackbook-pro.olin.edu>
2015-10-26 00:07:39 -0400

    Getting clue info from web database is working

---
+++ app/src/main/java/com/frantonlin/scavengerhunt/InfoCallback.java
@@ -0,0 +1,12 @@
+package com.frantonlin.scavengerhunt;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Interface for callbacks
+ * Created by Franton on 10/1/15
+ */
+public interface InfoCallback {
+    void callback(boolean success, HashMap<String, String> clueInfo);
+}
